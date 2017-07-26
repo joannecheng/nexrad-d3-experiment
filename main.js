6a360374e27ceab041969dfe4e35197c07815c64
Joanne Cheng <joanne@keen.io>
2017-07-26 08:51:34 -0600

    Use more common pattern for drawing on canvas

--- main.js
+++ main.js
@@ -38,18 +38,37 @@ radarQueue.await(drawRadar)
 
 // Draw Radar
 function drawRadar(error, radarData) {
-  const context = canvas.node().getContext('2d')
+  const context = canvas.node().getContext("2d")
+  const detachedContainer = document.createElement("custom");
+  const dataContainer = d3.select(detachedContainer);
+
+
   const radarColorScale = d3.scaleLinear()
     .domain([0, 10, 20, 30, 35, 40, 45, 50, 55])
-    .range(["#FFFFFF", "gray", "light blue", "#00FB90", "#00BB00", "#FFFF70", "#D0D060", "#FF6060", "#DA0000"]);
+    .range(["#FFFFFF", "gray", "light blue", "#00FB90", "#00BB00", "#FFFF70", "#D0D060", "#FF6060", "#DA0000"])
 
-  radarData.forEach((d) => {
+  const dataBinding = dataContainer.selectAll("custom.rect")
+    .data(radarData, (d) => d)
+
+  dataBinding.enter()
+    .append("custom")
+    .classed("rect", true)
+
+  drawCanvas(dataContainer, context, radarColorScale)
+}
+
+function drawCanvas(dataContainer, context, radarColorScale) {
+  const radarElements = dataContainer.selectAll('custom.rect')
+
+  radarElements.each((d) => {
     const val = parseFloat(d.value)
-    if (val === 0) { return }
-
     const lat = parseFloat(d.lat)
     const lon = parseFloat(d.lon)
+
+    if (val === 0) { return }
     const coords = projection([lon, lat])
+
+    console.log(coords);
 
     context.beginPath()
     context.rect(coords[0], coords[1], 1, 1)
