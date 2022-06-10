a3d94f517f2bc248c1674008c53a0f5197b17ec1
Joanne <chengjoanne2@gmail.com>
2022-06-10 07:23:43 -0400

    Updates

--- cljs/src/nexrad/main.cljs
+++ cljs/src/nexrad/main.cljs
@@ -1,4 +1,18 @@
-(ns nexrad.main)
+(ns nexrad.main
+  (:require [goog.dom :as dom]))
+
+(defn render-radar []
+  ;; TODO: this will render one radar file
+  )
+
+(defn draw-map []
+  ;; You could use the "." notation, but using goog.dom lets us skip that
+  ;; using google closure allows us to reduce
+  ;; dependencies and ensure compatibility with advanced mode compilation
+  (let [el (dom/getElement "viz")]
+    (prn "element" el)))
 
 (defn init []
-  (prn "test!!"))
+  (prn "test!!")
+
+  (draw-map))
