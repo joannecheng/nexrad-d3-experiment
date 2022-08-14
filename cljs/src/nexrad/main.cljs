07ce19a475a0d0ebbc35d0b623ee3c9281e0a62b
Joanne Cheng <chengjoanne2@gmail.com>
2022-08-14 13:28:25 -0400

    install reframe, topojson, start building basemap

--- cljs/src/nexrad/main.cljs
+++ cljs/src/nexrad/main.cljs
@@ -1,15 +1,21 @@
 (ns nexrad.main
-  (:require [goog.dom :as dom]
-            [uix.core.alpha :as uix :refer [defui]]
-            [uix.dom.alpha :as uix.dom]
-            [clojure.string :as str]
+  (:require [goog.object :as gobj]
+            [goog.dom :as dom]
+            [reagent.core :as r]
+            [reagent.dom :as rd]
+            [d3-geo :as d3-geo]
             [re-frame.core :as rf]))
 
 ;;-- Subscriptions
 (rf/reg-sub
  :radar-data
  (fn [db _]
-   (:data db)))
+   (:radar-data db)))
+
+(rf/reg-sub
+ :map-data
+ (fn [db _]
+   (:map-data db)))
 
 (rf/reg-sub
  :title
@@ -20,18 +26,24 @@
 (rf/reg-event-db
  :init-db
  (fn [_ _]
-   {:data []
+   {:radar-data []
+    :map-data []
     :title "Test title"}))
 
 (rf/reg-event-db
  :load-radar-data
  (fn [db [_ radar-data]]
-   (assoc db :data radar-data)))
+   (prn "loading radar dat" (first radar-data))
+   (assoc db :radar-data radar-data)))
 
-;;-- Components
+(rf/reg-event-db
+ :load-map-data
+ (fn [db [_ map-data]]
+   (assoc db :map-data map-data)))
 
-(def !csv-data (atom nil))
-(def !us-map (atom nil))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;-- Random Helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- clean-row [row]
   (-> row
@@ -40,42 +52,50 @@
       (update "value" js/parseFloat)))
 
 (defn- parse-csv [csv-body]
-  (let [all-rows (->> (str/split csv-body #"\r\n")
-                      (map #(str/split % #",")))]
-    (map #(-> (zipmap (first all-rows) %) clean-row)
-         (rest all-rows))))
-
-
-(defn render-radar [radar-data]
-  (prn "rendering!!!" (first radar-data))
-  radar-data)
+  (let [all-rows (->> (clojure.string/split csv-body #"\r\n")
+                      (map #(clojure.string/split % #",")))]
+    (mapv #(-> (zipmap (first all-rows) %) clean-row)
+          (rest all-rows))))
 
 (defn load-radar [filename]
   (-> (js/fetch filename)
       (.then #(.text %))
-      (.then #(reset! !csv-data %))
-      (.then #(-> @!csv-data parse-csv render-radar))))
+      (.then #(parse-csv %))
+      (.then #(rf/dispatch [:load-radar-data %]))))
 
 (defn load-us-map []
   (-> (js/fetch "us.json")
       (.then #(.text %))
       (.then #(.parse js/JSON %))
-      (.then #(reset! !us-map %))))
+      (.then #(rf/dispatch [:load-map-data %]))))
 
-(defn app []
-  (let [title @(rf/subscribe [:title])]
-    (prn "title" title)
-    [:div title]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Components
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn draw-radar []
-  (let [el (dom/getElement "viz")
-        radar (load-radar "radar/2017_08_25_KHGX_KHGX20170825_122025_V06.csv")]
-    (uix.dom/render [app] el)))
+(defn base-map []
+  (let [map-data @(rf/subscribe [:map-data])
+        projection (.geoAlbers ^js/Object d3-geo)
+        path (.geoPath d3-geo projection)]
+    (prn map-data)
+    (.log js/console (gobj/get map-data "feature"))
+    [:div "map data goes here"]))
 
-(defn draw-map []
-  (let [el (dom/getElement "viz")]
-    (load-us-map)))
+(defn radar-layer []
+  (let [radar-data @(rf/subscribe [:radar-data])]
+    (prn "radar data"  (first radar-data))
+    [:div (str "radar data here" (first radar-data))]))
+
+(defn App []
+  ;; Main components go here
+  (prn "rendering app")
+  [:div
+   [base-map]
+   [radar-layer]])
 
 (defn init []
-  (rf/dispatch-sync [:init-db])
-  (draw-map))
+  (let [el (dom/getElement "viz")]
+    (rf/dispatch-sync [:init-db])
+    (load-us-map)
+    (load-radar "radar/2017_08_25_KHGX_KHGX20170825_122025_V06.csv")
+    (rd/render [App] el)))
